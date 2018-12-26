026679c9fc6b6bf4217090bb825b7f11964a3f2c
joingue <joingue@awesomepiece.com>
2018-12-26 17:47:15 +0900

    Merge TransportLayer & NetworkEndpoint to NetworkEndpoint

--- NLIB/NetworkEndpoint.cpp
+++ NLIB/NetworkEndpoint.cpp
@@ -1,20 +1,45 @@
 #include "NetworkEndpoint.h"
 
+#include <boost/bind.hpp>
 #include <chrono>
+#include <cstdlib>
 
 #include "Utility.h"
+#include <iostream>
+
+using namespace boost::asio::ip;
 
 NetworkEndpoint::NetworkEndpoint()
 	: _thread(nullptr)
 	, _running(false)
-	, _transport(nullptr)
 {
 }
 
 void NetworkEndpoint::Startup(NetworkConfig& config)
 {
-	_transport = new TransportLayer();
-	_transport->Startup(config, this);
+	if (config.host != nullptr)
+	{
+		// TODO 좀 더 다듬기
+		udp::resolver resolver(_io_context);
+		char s_port[10];
+		snprintf(s_port, 10, "%d", config.port);
+		udp::resolver::query query(udp::v4(), config.host, s_port);
+		_remote_endpoint = *resolver.resolve(query).begin();
+
+		_socket = new udp::socket(_io_context, udp::endpoint(udp::v4(), 0));
+
+		Receive();
+	}
+	else
+	{
+		_socket = new udp::socket(_io_context, udp::endpoint(udp::v4(), config.port));
+
+		Receive();
+	}
+
+	_io_thread = new std::thread([this]() {
+		_io_context.run();
+	});
 
 	_thread = new std::thread([this]()
 	{
@@ -42,7 +67,14 @@ void NetworkEndpoint::Destroy()
 	delete _thread;
 	_thread = nullptr;
 
-	_transport->Destroy();
+	_io_context.stop();
+	_thread->join();
+	delete _thread;
+	_thread = nullptr;
+
+	delete _socket;
+	// _local_endpoint 가 더 상위 객체이므로 여기서 delete를 하진 않는다.
+	_local_endpoint = nullptr;
 	delete _transport;
 	_transport = nullptr;
 	*/
@@ -96,12 +128,92 @@ void NetworkEndpoint::HandleReceive(char* data, std::size_t length, NLIBAddress&
 
 void NetworkEndpoint::Send(NLIBAddress& address, const byte* data, uint32_t length)
 {
-	if (_transport)
-		_transport->Send(address, data, length);
+#ifdef NLIB_LOG_ENABLED
+	//std::cout << "[ Send ] " << std::endl;
+	//std::cout << Utility::ByteToString(data, length) << std::endl;
+#endif
+
+	_socket->async_send_to(
+		boost::asio::buffer(data, length)
+		, udp::endpoint(address::from_string(address.ip_str), address.port)
+		, boost::bind(&NetworkEndpoint::HandleSend
+			, this,
+			boost::asio::placeholders::error,
+			boost::asio::placeholders::bytes_transferred
+		)
+	);
 }
 
 void NetworkEndpoint::Send(NLIBAddress& address, UNLIBData data)
 {
-	if (_transport)
-		_transport->Send(address, std::move(data));
+	using namespace boost::asio;
+
+	std::vector<const_buffer> buffers;
+
+	NLIBData* tmp = data.get();
+	while (tmp != nullptr)
+	{
+		buffers.push_back(buffer(tmp->bytes, tmp->length));
+		tmp = tmp->next.get();
+}
+
+	_socket->async_send_to(
+		buffers
+		, udp::endpoint(address::from_string(address.ip_str), address.port)
+		, boost::bind(&NetworkEndpoint::HandleSend
+			, this,
+			placeholders::error,
+			placeholders::bytes_transferred
+		)
+	);
+}
+
+void NetworkEndpoint::HandleSend(const boost::system::error_code& error, std::size_t length)
+{
+#ifdef NLIB_LOG_ENABLED
+	std::cout << "[HandleSend] " << Utility::TimeInHHMMSSMMM() << std::endl;
+#endif
+
+	if (error)
+	{
+		std::cout << error.message() << std::endl;
+	}
+	assert(!error);
+}
+
+void NetworkEndpoint::Receive()
+{
+	_socket->async_receive_from(
+		boost::asio::buffer(_recv_buffer)
+		, _remote_endpoint
+		, boost::bind(
+			&NetworkEndpoint::HandleReceive
+			, this
+			, boost::asio::placeholders::error
+			, boost::asio::placeholders::bytes_transferred
+		)
+	);
+}
+
+void NetworkEndpoint::HandleReceive(const boost::system::error_code& error, std::size_t length)
+{
+#ifdef NLIB_LOG_ENABLED
+	std::cout << "[HandleRecv] " << Utility::TimeInHHMMSSMMM() << std::endl;
+#endif
+
+	assert(!error);
+	if (error)
+	{
+		std::cout << error.message() << std::endl;
+		return;
+	}
+
+	NLIBAddress address;
+	address.ip_str = _remote_endpoint.address().to_v4().to_string();
+	address.ip = _remote_endpoint.address().to_v4().to_ulong();
+	address.port = _remote_endpoint.port();
+
+	HandleReceive(_recv_buffer.data(), length, address);
+
+	Receive();
 }
--- NLIB/NetworkEndpoint.h
+++ NLIB/NetworkEndpoint.h
@@ -6,9 +6,11 @@
 #include <mutex>
 #include <cstdint>
 
+#include <boost/array.hpp>
+#include <boost/asio.hpp>
+
 #include "NetworkStruct.h"
 #include "NetworkConfig.h"
-#include "TransportLayer.h"
 #include "ByteStream.h"
 #include "ProtocolPacket.h"
 #include "BufferPool.h"
@@ -37,11 +39,23 @@ public:
 	virtual void Update(uint64_t time) = 0;
 	virtual void HandleReceive(NLIBRecv* data) = 0;
 
+	
 private:
+	// Asynchronous IO
+	void HandleSend(const boost::system::error_code& error, std::size_t length);
+
+	void Receive();
+	void HandleReceive(const boost::system::error_code& error, std::size_t length);
+
+private:
+	boost::asio::io_context _io_context;
+	boost::asio::ip::udp::socket* _socket;
+	boost::array<char, 2048> _recv_buffer;
+	boost::asio::ip::udp::endpoint _remote_endpoint;
+	std::thread* _io_thread;
+
 	std::thread* _thread;
 	bool _running;
-
-	TransportLayer* _transport;
 
 	std::mutex _recv_queue_mutex;
 	std::queue<NLIBRecv*> _recv_queue;
